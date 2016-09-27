bb6605bd115a05975b90956786cf69d9ec9da821
Carl Cortright <ckcortright@gmail.com>
2016-09-27 16:52:58 -0600

    Finished Stream iterators

--- C++/ContainersAndAlgorithms/iterators.cpp
+++ C++/ContainersAndAlgorithms/iterators.cpp
@@ -2,3 +2,31 @@
 * Some examples of using iterators
 */
 #include <algorithm>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <iterator>
+
+using namespace std;
+
+/*
+* An interesting example using input/output stream iterators
+*/
+int main(){
+  string from, to;
+  cin >> from >> to;
+
+  ifstream is {from};
+  istream_iterator<string> ii {is};
+  istream_iterator<string> eos {};
+
+  ofstream os {to};
+  ostream_iterator<string> oo {os, "\n"};
+
+  vector<string> b {ii, eos};
+  sort(b.begin(), b.end());
+
+  unique_copy(b.begin(), b.end(), oo);
+
+  return !is.eof() || !os;
+}
