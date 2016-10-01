b8c6504fae73636cfacaed52532729d55b36843e
Carl Cortright <ckcortright@gmail.com>
2016-09-30 19:19:55 -0600

    Started mutex

--- C++/Concurrency/SharedData.cpp
+++ C++/Concurrency/SharedData.cpp
@@ -1,4 +1,23 @@
 /*
-* Some examples of using mutex objects to prevent race conditions
+* Some examples of manipulating shared data using mutex objects
 */
 #include <mutex>
+
+mutex m;
+mutex m1;
+mutex m2;
+mutex m3;
+int sh;
+
+void f(){
+  unique_lock<mutex> lck {m};
+  sh += 7;
+} // The mutex gets released implicitly
+
+void f2(){
+  unique_lock<mutex> lck1 {m1, defer_lock};
+  unique_lock<mutex> lck1 {m2, defer_lock};
+  unique_lock<mutex> lck1 {m3, defer_lock};
+
+  lock(lck1, lck2, lck3); // Prevents process deadlock with multiple mutexes
+}
