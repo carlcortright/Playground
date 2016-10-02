ce16ef8d6417074dee0e7714ec789c55969d1a84
Carl Cortright <ckcortright@gmail.com>
2016-10-01 18:22:04 -0600

    Finished async tasks

--- C++/Concurrency/asyncs.cpp
+++ C++/Concurrency/asyncs.cpp
@@ -13,6 +13,9 @@ double accum(double* beg, double* end, double init){
   return accumulate(beg, end, init);
 }
 
+/*
+* Computes the sum of the vector v using async tasks
+*/
 double comp(vector<double>& v){
   // declare start value and size
   auto v0 = &v[0];
@@ -26,5 +29,5 @@ double comp(vector<double>& v){
   auto f2 = async(accum, v0+sz/2, v0+3*(sz/4), 0.0);
   auto f3 = async(accum, v0+3*(sz/4), v0+sz, 0.0);
 
-  return f0.get() + f1.get() + f2.get() + f3.get(); // return the sum of all
+  return f0.get() + f1.get() + f2.get() + f3.get();
 }
