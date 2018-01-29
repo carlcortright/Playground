7b38d71be9c575c7552a226444fe837122fad38a
Carl Cortright <ckcortright@gmail.com>
2018-01-29 14:25:27 -0700

    reading and writing files

---
+++ Ruby/Basics/rwfiles.rb
@@ -0,0 +1,14 @@
+# Some exampes of both reading and writing files
+filename = ARGV.first
+
+puts "Reading from #{filename}"
+
+# Open the file
+f = open(filename, 'w')
+
+# Truncate the file :( 
+f.truncate(0)
+
+f.write("This is a new line \n")
+f.write("hello!\n")
+f.write("goodbye!\n")
